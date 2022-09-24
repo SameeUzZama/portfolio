3a3bc89d7272b06bb412be514ac437d194949192
Samee Uz Zama <94825943+SameeUzZama@users.noreply.github.com>
2022-09-24 11:09:41 +0530

    update

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="h_left">
+        <div className="h_name">
+          <span>Hey! I Am</span>
+          <span>Samee Uz Zama</span>
+          <span>Details</span>
+        </div>
+      </div>
+      <div className="h_right"></div>
+    </div>
+  );
+}
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="n_wrapper">
+      <div className="n_left">
+        <div className="n_name">Samee Uz Zama</div>
+        <span>toggle</span>
+      </div>
+      <div className="n_right">
+        <div className="n_list">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+          </ul>
+        </div>
+        <button className="button n_button">Contact</button>
+      </div>
+    </div>
+  );
+}
