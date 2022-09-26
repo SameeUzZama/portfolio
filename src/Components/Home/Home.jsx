9d997f70d0d5a7938e8d8dbbd9088e05607f5c4d
Samee Uz Zama <94825943+SameeUzZama@users.noreply.github.com>
2022-09-26 19:00:54 +0530

    update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import "./Home.css";
+import Github from "../../Asset/github.png";
+import Linkedin from "../../Asset/linkedin.png";
+import Code from "../../Asset/code.png";
+import ThumbUp from "../../Asset/thumbup.png";
+import Glasses1 from "../../Asset/glassesimoji.png";
 
 export default function Home() {
   return (
@@ -7,10 +13,35 @@ export default function Home() {
         <div className="h_name">
           <span>Hey! I Am</span>
           <span>Samee Uz Zama</span>
-          <span>Details</span>
+          <span>
+            React JS developer who is for designing and implementing UI
+            components for JavaScript-based web applications with the use of
+            open- source library infrastructure. My passion became my
+            profession, I'm a driven, energetic and proactive tech
+            professionally.
+          </span>
+        </div>
+        <button className="button h_button">Hire Me</button>
+        <div className="h_icons">
+          <a
+            href="https://github.com/SameeUzZama"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Github} alt="" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/samee-uz-zama/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Linkedin} alt="" />
+          </a>
         </div>
       </div>
-      <div className="h_right"></div>
+      <div className="h_right">
+        <img src={Code} alt="" />
+      </div>
     </div>
   );
 }
